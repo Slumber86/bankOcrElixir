6a5e91a8ff5bf3ec40df696ed3d9d501e4b3da55
Simon Vocella <voxsim@gmail.com>
2016-05-21 16:10:51 +0200

    add one more test

--- test/digitrecognizer_test.exs
+++ test/digitrecognizer_test.exs
@@ -3,10 +3,17 @@ defmodule DigitRecognizerTest do
   doctest DigitRecognizer
 
   test "all zeroes" do
-    assert DigitRecognizer.parse_triple(
-      " _  _  _  _  _  _  _  _  _ ",
-      "| || || || || || || || || |",
-      "|_||_||_||_||_||_||_||_||_|") == "000000000"
+    assert DigitRecognizer.parse(
+      " _  _  _  _  _  _  _  _  _ \n" <>
+      "| || || || || || || || || |\n" <>
+      "|_||_||_||_||_||_||_||_||_|\n") == "000000000"
+  end
+
+  test "all ones" do
+    assert DigitRecognizer.parse(
+      "                           \n" <>
+      "  |  |  |  |  |  |  |  |  |\n" <>
+      "  |  |  |  |  |  |  |  |  |\n") == "111111111"
   end
 
   test "all zeroes" do
