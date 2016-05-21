48e6a73d9e732a798d53a2eea91d33e0fa528ea3
Agostino Marzotta <ago1986@gmail.com>
2016-05-21 16:20:52 +0200

    Recognizer

--- lib/digitrecognixer.ex
+++ lib/digitrecognixer.ex
@@ -1,21 +1,28 @@
 defmodule DigitRecognizer do
+  def parse(inp) do
+    [top,middle,bottom,_] = String.split(inp, "\n")
+    parse_triple(top, middle, bottom)
+  end
   def parse_triple(top, middle, bottom) do
-    top_list = String.split(top, "", parts: 7)
+    top_list = Enum.reduce(9..1, [], fn (x,acc) -> [String.slice(top, (x-1)*3..(x*3)-1) | acc] end)
+    middle_list = Enum.reduce(9..1, [], fn (x,acc) -> [String.slice(middle, (x-1)*3..(x*3)-1) | acc] end)
+    bottom_list = Enum.reduce(9..1, [], fn (x,acc) -> [String.slice(bottom, (x-1)*3..(x*3)-1) | acc] end)
+    inner_parse(top_list, middle_list, bottom_list)
   end
 
-  def inner_parse(<<a, b, c, rest :: binary>>, list) do
-    inner_parse(rest, [a<>b<>c | list] )
+  def inner_parse([t | top], [m | middle], [b | bottom]) do
+    _parse_digit(t,m,b) <> inner_parse(top,middle,bottom)
   end
-  def inner_parse(_, list) do
-    list
+  def inner_parse([], [], []) do
+    ""
   end
   def parse_digit(a,b,c) do
     _parse_digit(a,b,c)
   end
   defp _parse_digit(" _ ", "| |", "|_|") do
-    0
+    "0"
   end
   defp _parse_digit("   ", "  |", "  |") do
-    1
+    "1"
   end
 end
